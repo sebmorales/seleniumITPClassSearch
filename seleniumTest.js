c8a559cc7544d906ea9c3f69297f6ac771c36f30
sebmorales <sebmoralesprado@gmail.com>
2017-06-24 15:27:15 +0200

    added Emailer, removed unnecesarry requests

--- seleniumTest.js
+++ seleniumTest.js
@@ -5,13 +5,41 @@
 //  changes.
 //
 ////////////////////////////////////////////////////////////////
-
+'use strict';
+const nodemailer = require('nodemailer');
 var webdriver=require('selenium-webdriver');
 var driver=new webdriver.Builder().forBrowser('chrome').build();
-navigateClass(83);//liveWeb
-navigateClass(526);//understanding Networks
-navigateClass(451);//basic analog circuits
-navigateClass(457);//liveWeb
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
+    auth: {
+        user: 'sebnodejs@gmail.com',
+        pass: 'sebnodejsITP'
+    }
+});
+
+let mailOptions = {
+    from: '"Seb ITP Classes" <sebnodejs@gmail.com>', // sender address
+    to: 'sebmoralesprado@gmail.com',
+    subject: 'class status changed! ', // Subject line
+    text: 'log into your nyu acount', // plain text body
+    html: '<b>log into your nyu acount</b>' // html body
+};
+
+foreverCheck();
+
+function foreverCheck(){
+
+  navigateClass(457);//liveWeb
+  navigateClass(526);//understanding Networks
+  setTimeout(function(){foreverCheck();}, 120000);
+
+// navigateClass(451);//basic analog circuits
+// navigateClass(83);//liveWeb
+}
 
 function navigateClass(classNumber){
   var classStatusXpath;
@@ -22,27 +50,27 @@ function navigateClass(classNumber){
   }
   var classXpath='//*[@id="NYU_CLS_DERIVED_TERM$'+classNumber+'"]/img';
 
-  //check if we are already in the itp class page.
-  // driver.isElementPresent({xpath: classXpath}).then(console.log("Yey"));
-
-
-  // driver.get('https://sis.nyu.edu/psc/csprod/EMPLOYEE/CSSS/c/NYU_SR.NYU_CLS_SRCH.GBL?Page=NYU_CLS_SRCH&Action=U&ExactKeys=Y&TargetFrameName=None');
-  // driver.manage().timeouts().pageLoadTimeout(2000);
-  // driver.executeScript('javascript:submitAction_win0(document.win0,"LINK1$368");');
-  // driver.manage().timeouts().pageLoadTimeout(5000);
-  // console.log("error Found");
-  //
-
-
-  driver.get('https://sis.nyu.edu/psc/csprod/EMPLOYEE/CSSS/c/NYU_SR.NYU_CLS_SRCH.GBL?Page=NYU_CLS_SRCH&Action=U&ExactKeys=Y&TargetFrameName=None');
-  driver.manage().timeouts().pageLoadTimeout(4000);
-  driver.executeScript('javascript:submitAction_win0(document.win0,"LINK1$368");');
-  driver.manage().timeouts().pageLoadTimeout(10000);
-  driver.wait(webdriver.until.elementLocated({xpath:classXpath}),20000);
-  driver.findElement({xpath: classXpath}).click();
-  driver.wait(webdriver.until.elementLocated({xpath:classStatusXpath}),20000);
-  // var elem=driver.findElement(webdriver.By.xpath('//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$461"]/div/table/tbody/tr/td/span[5]')).getText().then(function(text){console.log(text);});
-  var elem=driver.findElement(webdriver.By.xpath(classStatusXpath)).getText().then(function(text){checkClassStatus(text,classNumber);});
+  // checkExistance(classXpath,classStatusXpath,classNumber);
+  driver.findElement({xpath: classXpath}).then(function(webElement) {
+        driver.findElement({xpath: classXpath}).click();
+        driver.wait(webdriver.until.elementLocated({xpath:classStatusXpath}),20000);
+        // var elem=driver.findElement(webdriver.By.xpath('//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$461"]/div/table/tbody/tr/td/span[5]')).getText().then(function(text){console.log(text);});
+        var elem=driver.findElement(webdriver.By.xpath(classStatusXpath)).getText().then(function(text){checkClassStatus(text,classNumber);});
+    }, function(err) {
+        // if (err.state == 'no such element') {
+        //     console.log('Element not found');
+        // } else {
+          driver.get('https://sis.nyu.edu/psc/csprod/EMPLOYEE/CSSS/c/NYU_SR.NYU_CLS_SRCH.GBL?Page=NYU_CLS_SRCH&Action=U&ExactKeys=Y&TargetFrameName=None');
+          driver.manage().timeouts().pageLoadTimeout(4000);
+          driver.executeScript('javascript:submitAction_win0(document.win0,"LINK1$368");');
+          driver.manage().timeouts().pageLoadTimeout(10000);
+          driver.wait(webdriver.until.elementLocated({xpath:classXpath}),20000);
+          driver.findElement({xpath: classXpath}).click();
+          driver.wait(webdriver.until.elementLocated({xpath:classStatusXpath}),20000);
+          // var elem=driver.findElement(webdriver.By.xpath('//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$461"]/div/table/tbody/tr/td/span[5]')).getText().then(function(text){console.log(text);});
+          var elem=driver.findElement(webdriver.By.xpath(classStatusXpath)).getText().then(function(text){checkClassStatus(text,classNumber);});
+        // }
+    });
 }
 
 function checkClassStatus(status,classNumber){
@@ -60,46 +88,13 @@ function checkClassStatus(status,classNumber){
   }
   if(status[0]=='W'){
     console.log("Class "+className+" now waitlisted!!!");
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
   }
   //else{console.log(classNumber+" "+status);}
-
 }
-
-
-// var classXpath='//*[@id="NYU_CLS_DERIVED_TERM$'+liveWeb+'"]/img';
-// var classStatusXpath='//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$'+(liveWeb+4)+'"]/div/table/tbody/tr/td/span[5]'
-//
-// driver.get('https://sis.nyu.edu/psc/csprod/EMPLOYEE/CSSS/c/NYU_SR.NYU_CLS_SRCH.GBL?Page=NYU_CLS_SRCH&Action=U&ExactKeys=Y&TargetFrameName=None');
-// driver.manage().timeouts().pageLoadTimeout(2000);
-// driver.executeScript('javascript:submitAction_win0(document.win0,"LINK1$368");');
-// driver.manage().timeouts().pageLoadTimeout(5000);
-// driver.wait(webdriver.until.elementLocated({xpath:classXpath}),10000);
-// driver.findElement({xpath: classXpath}).click();
-// driver.wait(webdriver.until.elementLocated({xpath:classStatusXpath}),15000);
-// // var elem=driver.findElement(webdriver.By.xpath('//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$461"]/div/table/tbody/tr/td/span[5]')).getText().then(function(text){console.log(text);});
-// var elem=driver.findElement(webdriver.By.xpath(classStatusXpath)).getText().then(function(text){checkClassStatus(text);});
-
-
-
-
-
-
-
-//.then(function(innerHTML){consol.log(innerHTML)};);
-//console.log(elem);
-//var myElement = element(by.css('.myclass'));
-//
-// elem.getInnerHtml().then(function(html) {
-//   console.log(html);
-//     //do stuff with html here
-// });
-
-//var elemInner=elem.getAttribute("innerHTML").then(function(getAttribute){console.log(elemInner);});
-
-//*[@id="NYU_CLS_DERIVED_TERM$457"]/img
-//driver.findElement(webdriver.By.name('q')).sendKeys('helo world');
-// driver.findElement({name:'q'}).sendKeys(webdriver.Key.ENTER);
-// driver.wait(webdriver.until.elementLocated({xpath:'//*[@id="rso"]/div[2]/div/div[1]/div/div/h3/a'}),3000);
-// driver.findElement({xpath: '//*[@id="rso"]/div[2]/div/div[1]/div/div/h3/a'}).click();
- // driver.getTitle().then(function(title){console.log(title);});
-// driver.quit();
