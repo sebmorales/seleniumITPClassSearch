df6dffc40d91712868f16335ab176794683e7d7e
sebmorales <sebmoralesprado@gmail.com>
2017-08-05 12:14:36 -0400

    fixed bug that made it crash, added classes as objects and reorganized functions to be more readable

--- seleniumTest.js
+++ seleniumTest.js
@@ -7,7 +7,10 @@
 ////////////////////////////////////////////////////////////////
 'use strict';
 const nodemailer = require('nodemailer');
-var webdriver=require('selenium-webdriver');
+var webdriver=require('selenium-webdriver'),
+By = webdriver.By;
+var phantomjs = require('phantomjs')
+
 var driver=new webdriver.Builder().forBrowser('chrome').build();
 
 // create reusable transporter object using the default SMTP transport
@@ -29,72 +32,62 @@ let mailOptions = {
     html: '<b>log into your nyu acount</b>' // html body
 };
 
-foreverCheck();
+let classes=[{
+  name: 'Programming from A-Z',
+  number: '269'
+},
+{
+  name: 'NIME',
+  number: '105'
+},
+{
+  name: 'Understanding Networks',
+  number: '526'
+}
+];
+
+navigateDepartment();
 
 function foreverCheck(){
+  navigateClass(classes[0]);//Programming A-Z 269
+  navigateClass(classes[1]);//Programming A-Z 269
+  navigateClass(classes[2]);//Programming A-Z 269
 
-  navigateClass(457);//liveWeb
-  navigateClass(526);//understanding Networks
-  setTimeout(function(){foreverCheck();}, 120000);
+  // navigateClass(457);//liveWeb
+  // navigateClass(526);//understanding Networks
+  setTimeout(function(){foreverCheck();}, 40000);
 
 // navigateClass(451);//basic analog circuits
 // navigateClass(83);//liveWeb
 }
 
-function navigateClass(classNumber){
-  var classStatusXpath;
-  if (classNumber<100){
-    var classStatusXpath='//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$'+(classNumber+1)+'"]/div/table/tbody/tr/td/span[5]';
-  }else{
-    classStatusXpath='//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$'+(classNumber+4)+'"]/div/table/tbody/tr/td/span[5]';
-  }
-  var classXpath='//*[@id="NYU_CLS_DERIVED_TERM$'+classNumber+'"]/img';
-
-  // checkExistance(classXpath,classStatusXpath,classNumber);
-  driver.findElement({xpath: classXpath}).then(function(webElement) {
-        driver.findElement({xpath: classXpath}).click();
-        driver.wait(webdriver.until.elementLocated({xpath:classStatusXpath}),20000);
-        // var elem=driver.findElement(webdriver.By.xpath('//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$461"]/div/table/tbody/tr/td/span[5]')).getText().then(function(text){console.log(text);});
-        var elem=driver.findElement(webdriver.By.xpath(classStatusXpath)).getText().then(function(text){checkClassStatus(text,classNumber);});
-    }, function(err) {
-        // if (err.state == 'no such element') {
-        //     console.log('Element not found');
-        // } else {
-          driver.get('https://sis.nyu.edu/psc/csprod/EMPLOYEE/CSSS/c/NYU_SR.NYU_CLS_SRCH.GBL?Page=NYU_CLS_SRCH&Action=U&ExactKeys=Y&TargetFrameName=None');
-          driver.manage().timeouts().pageLoadTimeout(4000);
-          driver.executeScript('javascript:submitAction_win0(document.win0,"LINK1$368");');
-          driver.manage().timeouts().pageLoadTimeout(10000);
-          driver.wait(webdriver.until.elementLocated({xpath:classXpath}),20000);
-          driver.findElement({xpath: classXpath}).click();
-          driver.wait(webdriver.until.elementLocated({xpath:classStatusXpath}),20000);
-          // var elem=driver.findElement(webdriver.By.xpath('//*[@id="win0divNYU_CLS_DERIVED_HTMLAREA3$461"]/div/table/tbody/tr/td/span[5]')).getText().then(function(text){console.log(text);});
-          var elem=driver.findElement(webdriver.By.xpath(classStatusXpath)).getText().then(function(text){checkClassStatus(text,classNumber);});
-        // }
-    });
+function navigateDepartment() {
+  driver.get('https://sis.nyu.edu/psc/csprod/EMPLOYEE/CSSS/c/NYU_SR.NYU_CLS_SRCH.GBL?Page=NYU_CLS_SRCH&Action=U&ExactKeys=Y&TargetFrameName=None');
+  driver.manage().timeouts().pageLoadTimeout(4000);
+  driver.executeScript('javascript:submitAction_win0(document.win0,"LINK1$364");');//go to ITP
+  driver.manage().timeouts().pageLoadTimeout(10000);
+  driver.wait(webdriver.until.elementLocated({id: 'win0divNYU_CLS_DERIVED_TERM$105'}),19000);//check for nime existance (just make sure you are there)
+  foreverCheck();
 }
 
-function checkClassStatus(status,classNumber){
-  var className;
-  if(classNumber==83){className="Sketching Communities";}
-  if(classNumber==526){className="Understanding Networks";}
-  if(classNumber==451){className="Basic Analog Circuits";}
-  if(classNumber==457){className="LiveWeb";}
+function clickClass(classNo){
+    driver.findElement({xpath: '//*[@id="NYU_CLS_DERIVED_TERM$'+classNo+'"]/img'}).click();
+}
 
-  if(status=="Closed"){
-    console.log("Class "+className+" still closed");
-  }
-  if(status=="Open"){
-    console.log("Class "+className+" now open!!!");
-  }
-  if(status[0]=='W'){
-    console.log("Class "+className+" now waitlisted!!!");
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-    });
-  }
-  //else{console.log(classNumber+" "+status);}
+function navigateClass(classObject){
+  //var classXpath='//*[@id="NYU_CLS_DERIVED_TERM$'+classObject.number+'"]/img';
+  var classID= 'win0divNYU_CLS_DERIVED_TERM$'+classObject.number;
+  //console.log(classID);
+  driver.findElement(By.id(classID)).getText().then(function(txt) {
+  //  console.log(txt);
+    if(txt.indexOf("Class Status: Open") !== -1){
+      console.log(classObject.name+" is open");
+    }else if(txt.indexOf("Class Status: Wait List") !== -1){
+      console.log(classObject.name+" is Wait List");
+    }else if (txt.indexOf("Class Status: Closed")!==-1){
+      console.log(classObject.name+" is closed");
+    }else{
+      setTimeout(function(){clickClass(classObject.number);},3000);
+    }
+  }, function(err) { });
 }
